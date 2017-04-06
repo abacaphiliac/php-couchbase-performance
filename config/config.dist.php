a864f1fef37341283eb288b4247e96f55e8406eb
Tim Younger <tim@webpt.com>
2017-04-06 09:15:18 -0700

    expose env vars for config values (to use in docker container)

--- config/config.dist.php
+++ config/config.dist.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'couchbase_dsn' => 'couchbase://localhost',
-    'couchbase_bucket' => 'default',
-    'fetch_per_insert' => 2,
-    'ttl' => 60,
-    'memcache_servers' => 'localhost:11211',
+    'couchbase_dsn' => getenv('TEST_COUCHBASE_DSN') ?: 'couchbase://localhost',
+    'couchbase_bucket' => getenv('TEST_COUCHBASE_BUCKET') ?: 'default',
+    'fetch_per_insert' => getenv('TEST_FETCH_PER_INSERT') ?: 2,
+    'ttl' => getenv('TEST_TTL') ?: 60,
+    'memcache_servers' => getenv('TEST_MEMCACHE_SERVERS') ?: 'localhost:11211',
 ];
